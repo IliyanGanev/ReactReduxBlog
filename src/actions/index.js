5e60ff7148eae6922d97ba03c3c77a6b91f08666
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-21 23:16:11 -0800

    Render a list of posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog/herokuapp.com/api';
-const API_KEY = '?key=botilko123';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=PAPERCLIP12345';
 
 export function fetchPosts() {
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
